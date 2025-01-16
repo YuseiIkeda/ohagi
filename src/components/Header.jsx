281864cb2941776a35e57e5c87dc94e7550d22c5
YuseiIkeda <chys22020@g.nihon-u.ac.jp>
2025-01-16 16:10:44 +0900

    最終

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
         <header className="header">
             <img src="/images/logo.png" alt="ロゴ" className="header-logo" />
-            <h1 className="header-title">アプリケーション紹介サイト</h1>
+            <h1 className="header-title">アプリケーション紹介サイト ~ 日大文理チュートリアル </h1>
         </header>
     );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -35,7 +35,7 @@ export default function Main() {
                     <iframe
                         width="560"
                         height="315"
-                        src="https://www.youtube.com/embed/pQevvTMI2bc"
+                        src="https://www.youtube.com/watch?v=U1FJW_bfNUI"
                         title="YouTube video player"
                         frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
